37ade3f1b752a1ca7607ce881ca87a10338757d1
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-03-07 14:17:26 -0300

    text tags - crate H1, H2, H3, H4, H5, H6, B, I, P AND TITLE tags

---
+++ tags/text.py
@@ -0,0 +1,63 @@
+from typing import Union, List
+
+from essential.tag import Tag
+
+
+class H1(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h1', inner, True, **kwargs)
+
+
+class H2(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h2', inner, True, **kwargs)
+
+
+class H3(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h3', inner, True, **kwargs)
+
+
+class H4(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h4', inner, True, **kwargs)
+
+
+class H5(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h5', inner, True, **kwargs)
+
+
+class H6(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('h6', inner, True, **kwargs)
+
+
+class B(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('b', inner, True, **kwargs)
+
+
+class I(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('i', inner, True, **kwargs)
+
+
+class P(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('p', inner, True, **kwargs)
+
+
+class TITLE(Tag):
+
+    def __init__(self, inner: str, **kwargs):
+        super().__init__('title', inner, True, **kwargs)
