424ee8b3cc81ec1b80740cbf9a812d85b2ba84cf
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-08 17:49:17 -0300

    factory  - create BaseFactory class to create base classes in base.py file

---
+++ pyhtml/dtypes/factory.py
@@ -0,0 +1,20 @@
+from ..dtypes.base import Element, HeadTag, BodyTag, NotRecordableTag
+
+
+class BaseFactory:
+
+    @staticmethod
+    def create_element_class():
+        return Element()
+
+    @staticmethod
+    def create_head_tag_class(name: str, is_closed: bool):
+        return HeadTag(name, is_closed)
+
+    @staticmethod
+    def create_body_tag_class(name: str, is_closed: bool):
+        return BodyTag(name, is_closed)
+
+    @staticmethod
+    def create_not_recordable_tag_class(name: str, is_closed: bool):
+        return NotRecordableTag(name, is_closed)
