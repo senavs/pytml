6f7d9700e117fa4e59055214fd2381e3bd8ecca8
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-08 16:02:31 -0300

    base classes  - create Tag class

--- dtypes/base.py
+++ dtypes/base.py
@@ -26,3 +26,39 @@ class Element:
 
     def __str__(self):
         return self.render()
+
+
+class Tag(Element):
+    _element_fmr = '<{name}{attributes}>{inner}'
+    __name = __attributes = ''
+
+    def __init__(self, name: str, inner: Union[str, 'Element', List['Element']], is_closed: bool, **kwargs):
+        super().__init__(inner)
+        if is_closed:
+            self._element_fmr += '</{name}>'
+
+        self.name = name
+        self.attributes = kwargs
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        self.__name = value
+
+    @property
+    def attributes(self) -> str:
+        return self.__attributes
+
+    @attributes.setter
+    def attributes(self, kwargs: dict):
+        if kwargs:
+            for key, value in kwargs.items():
+                if str(key).endswith('_'):
+                    key = key[:-1]
+                self.__attributes += f' {key}="{value}"'
+
+    def render(self) -> str:
+        return self._element_fmr.format(name=self.name, attributes=self.attributes, inner=self.inner)
